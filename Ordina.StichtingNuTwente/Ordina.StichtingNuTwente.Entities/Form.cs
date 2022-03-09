1e75df806017830a571bca90c7f9fce6323d85a4
Bas Plijnaer <basplijnaer@hotmail.com>
2022-03-09 21:26:27 +0100

    Added Json testfile

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Entities/Form.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Entities/Form.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Header { get; set; } //Text inderneath the form title
+        public string Header { get; set; } //Text underneath the form title
         public List<Section> Sections { get; set; }
     }
 }
--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Entities/Question.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Entities/Question.cs
@@ -7,5 +7,7 @@
         public string Text { get; set; }
         public string Description { get; set; }
         public string InputType { get; set; } //Input type like Textbox, Selection List
+        public List<String> InputOptions { get; set; }
+        public bool Required { get; set; }
     }
 }
