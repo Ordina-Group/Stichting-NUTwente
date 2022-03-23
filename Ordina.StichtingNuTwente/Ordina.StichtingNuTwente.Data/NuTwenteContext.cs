057cde5ed0b8ff699b634978c34097ade23cf812
Bart Klomp <Bart.Klomp@ordina.nl>
2022-03-23 14:03:54 +0100

    Added userdetail class

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Data/NuTwenteContext.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Data/NuTwenteContext.cs
@@ -12,7 +12,7 @@ namespace Ordina.StichtingNuTwente.Data
     {
 
 
-        public NuTwenteContext(DbContextOptions<NuTwenteContext> options): base(options)
+        public NuTwenteContext(DbContextOptions<NuTwenteContext> options) : base(options)
         {
             this.Database.EnsureCreated();
             this.Database.Migrate();
@@ -25,6 +25,9 @@ namespace Ordina.StichtingNuTwente.Data
             modelBuilder.Entity<Gastgezin>().HasMany(x => x.Vluchtelingen).WithOne(x => x.Gastgezin);
             modelBuilder.Entity<Gastgezin>().HasOne(g => g.Begeleider);
             modelBuilder.Entity<Gastgezin>().HasOne(g => g.Contact);
+            modelBuilder.Entity<UserDetails>().Property(u => u.Roles).HasConversion(
+                v => string.Join(',', v),
+                v => v.Split(',', StringSplitOptions.RemoveEmptyEntries));
         }
 
         public DbSet<Vrijwilliger> Vrijwilligers { get; set; }
@@ -38,5 +41,6 @@ namespace Ordina.StichtingNuTwente.Data
         public DbSet<Adres> Adres { get; set; }
 
         public DbSet<Persoon> Persoon { get; set; }
+        public DbSet<UserDetails> Users { get; set; }
     }
 }
