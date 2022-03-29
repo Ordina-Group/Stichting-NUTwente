b2cca70117633e746f009f50e5f391d6a0337072
Bart Klomp <Bart.Klomp@ordina.nl>
2022-03-29 11:59:33 +0200

    Added vrijwilliger datatype

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Entities/Form.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Entities/Form.cs
@@ -1,4 +1,6 @@
-﻿namespace Ordina.StichtingNuTwente.Entities
+﻿using Ordina.StichtingNuTwente.Models.Models;
+
+namespace Ordina.StichtingNuTwente.Entities
 {
     public class Form
     {
@@ -6,5 +8,8 @@
         public string Title { get; set; }
         public string Header { get; set; } //Text underneath the form title
         public List<Section> Sections { get; set; }
+
+        public UserDetails? UserDetails { get; set; }
+        public List<UserDetails> AllUsers { get;} = new List<UserDetails>();
     }
 }
