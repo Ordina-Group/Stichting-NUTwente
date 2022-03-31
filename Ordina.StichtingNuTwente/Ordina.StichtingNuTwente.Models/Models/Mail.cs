30e3b924e356ba3ae4282796970436e0806b919f
NiekNieuwenhuisen <niek.nieuwenhuisen@hotmail.com>
2022-03-31 15:43:45 +0200

    Back-end mailservice

---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Models/Models/Mail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordina.StichtingNuTwente.Models.Models
+{
+    public class Mail : BaseEntity
+    {
+        public string Message { get; set; }
+        public string Subject { get; set; }
+        public string MailToAdress { get; set; }
+        public string MailToName { get; set; }
+        public string MailFromName { get; set; }
+    }
+}
