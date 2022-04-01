ab16c11f4dc36c46b6345ce687325d931d8510f6
Bas Plijnaer <basplijnaer@hotmail.com>
2022-04-01 15:39:45 +0200

    added confirmation email

---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Business/Interfaces/IPersoonService.cs
@@ -0,0 +1,11 @@
+﻿using Ordina.StichtingNuTwente.Data;
+using Ordina.StichtingNuTwente.Models.Models;
+
+namespace Ordina.StichtingNuTwente.Business.Interfaces
+{
+    public interface IPersoonService
+    {
+        public Persoon? GetPersoon(int id);
+        public Persoon? GetPersoonByReactieId(int? reactionId);
+    }
+}
---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Business/Services/PersoonService.cs
@@ -0,0 +1,27 @@
+﻿using Ordina.StichtingNuTwente.Business.Interfaces;
+using Ordina.StichtingNuTwente.Data;
+using Ordina.StichtingNuTwente.Models.Models;
+
+namespace Ordina.StichtingNuTwente.Business.Services
+{
+    public class PersoonService: IPersoonService
+    {
+        private readonly NuTwenteContext _context;
+        public PersoonService(NuTwenteContext context)
+        {
+            _context = context;
+        }
+
+        public Persoon? GetPersoon(int id)
+        {
+            var persoonRepository = new Repository<Persoon>(_context);
+            return persoonRepository.GetById(id);
+        }
+
+        public Persoon? GetPersoonByReactieId(int? reactieId)
+        {
+            var persoonRepository = new Repository<Persoon>(_context);
+            return persoonRepository.GetFirstOrDefault(p => p.Reactie.Id == reactieId);
+        }
+    }
+}
