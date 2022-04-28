debaff3de131004961d597f40cdd628d8acbefc2
NiekNieuwenhuisen <niek.nieuwenhuisen@hotmail.com>
2022-04-28 11:09:29 +0200

    Restant merge die weg mag

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Business/Interfaces/IPersoonService.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Business/Interfaces/IPersoonService.cs
@@ -5,8 +5,6 @@ namespace Ordina.StichtingNuTwente.Business.Interfaces
 {
     public interface IPersoonService
     {
-        public Persoon? getPersoon(int id);
-        public Persoon? getPersoonByReactionId(int reactionId);
         public Persoon? GetPersoon(int id);
         public Persoon? GetPersoonByReactieId(int? reactionId);
         public ICollection<Persoon> GetAllPersonen();
