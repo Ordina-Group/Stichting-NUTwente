57f2b5ad54f637c25432128fe19347d136aeda8f
Bart Klomp <bartpklomp@gmail.com>
2022-05-03 11:35:10 +0200

    Added indropdown checkbox andsorted vrijwilligers by firstname

---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Data/Migrations/20220503084926_AddedDropdownBool.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordina.StichtingNuTwente.Data.Migrations
+{
+    public partial class AddedDropdownBool : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "InDropdown",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InDropdown",
+                table: "Users");
+        }
+    }
+}
