70506f93d049000d2a05b8d60d89fb73748ced13
Bart Klomp <bartpklomp@gmail.com>
2022-05-30 10:54:12 +0200

    Added soft delete

---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Data/Migrations/20220530084159_SoftDelete.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordina.StichtingNuTwente.Data.Migrations
+{
+    public partial class SoftDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Reacties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Gastgezinnen",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Reacties");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Gastgezinnen");
+        }
+    }
+}
