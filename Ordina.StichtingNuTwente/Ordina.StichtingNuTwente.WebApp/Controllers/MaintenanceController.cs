5cacef0bb84e14bab0ca518206a58ccc8f0fab38
Bart Klomp <bartpklomp@gmail.com>
2022-06-08 12:57:10 +0200

    Fixed maintenance overview for fix status

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.WebApp/Controllers/MaintenanceController.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.WebApp/Controllers/MaintenanceController.cs
@@ -406,9 +406,15 @@ namespace Ordina.StichtingNuTwente.WebApp.Controllers
 
         public IActionResult FixStatus()
         {
-            maintenanceService.UpdateStatus();
-
-            return Ok();
+            var model = new MaintenanceModel();
+            var messages = maintenanceService.UpdateStatus();
+            model.Messages = new List<MaintenanceMessage>();
+            model.Messages.AddRange(messages.Select(x => new MaintenanceMessage
+            {
+                Message = x.Message,
+                MessageType = (MaintenanceMessageType)x.MessageType
+            }));
+            return View("Index", model);
         }
     }
 }
