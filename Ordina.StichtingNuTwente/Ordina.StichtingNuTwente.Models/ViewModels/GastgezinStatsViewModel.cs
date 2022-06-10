be4475fc1e96cc37886fc357881387717ece49c0
Bart Klomp <bartpklomp@gmail.com>
2022-06-10 11:30:22 +0200

    Added api endpoint voor counter

---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Models/ViewModels/GastgezinStatsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordina.StichtingNuTwente.Models.ViewModels
+{
+    public class GastgezinStatsViewModel
+    {
+
+        public int AantalVluchtelingenGeplaatst { get; set; }
+        public int VerdeeldOverAantalGastgezinnen { get; set; }
+
+        public GastgezinStatsViewModel(int aantalVluchtelingenGeplaatst, int verdeeldOverAantalGastgezinnen)
+        {
+            AantalVluchtelingenGeplaatst = aantalVluchtelingenGeplaatst;
+            VerdeeldOverAantalGastgezinnen = verdeeldOverAantalGastgezinnen;
+        }
+
+    }
+}
