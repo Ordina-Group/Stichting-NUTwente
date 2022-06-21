3800642eacfcfd783fb8e680616f1b4c25dcc875
Bas Plijnaer <basplijnaer@hotmail.com>
2022-06-21 11:40:26 +0200

    migration

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Models/Models/PlaatsingsInfo.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Models/Models/PlaatsingsInfo.cs
@@ -11,8 +11,7 @@ namespace Ordina.StichtingNuTwente.Models.Models
     {
         public PlaatsingsInfo()
         {
-            Belemmering = "";
-            KleineKinderen = "";
+            KleineKinderen  = "";
             VolwassenenGrotereKinderen = "";
             SlaapkamerRuimte = "";
             Privacy = "";
@@ -22,19 +21,24 @@ namespace Ordina.StichtingNuTwente.Models.Models
             KoelkastRuimte = "";
             DaglichtSlaapkamer = "";
             Roken = "";
-            AlchoholEnDrugs = "";
             VeiligeOpbergruimte = "";
             HuisdierenAanwezig = "";
             HuisdierenMogelijk = "";
             Allergieen = "";
             VluchtelingOphalen = "";
             BasisscholenAanwezig = "";
-            KinderenInDeBuurt = "";
-            FaciliteitenVoorKinderen = "";
+            Beperkingen = "";
+            AdresVanLocatie = "";
+            PostcodeVanLocatie = "";
+            PlaatsnaamVanLocatie = "";
             OverigeOpmerkingen = "";
+            GezinsSamenstelling = "";
+            TelefoonnummerVanLocatie = "";
+            GezinsLeeftijden = "";
+            SlaapplaatsOpmerking = "";
+            EigenToegangsdeur = "";
         }
 
-        public string? Belemmering { get; set; }
         public string? KleineKinderen { get; set; }
         public string? VolwassenenGrotereKinderen { get; set; }
         public string? SlaapkamerRuimte { get; set; }
@@ -45,15 +49,12 @@ namespace Ordina.StichtingNuTwente.Models.Models
         public string? KoelkastRuimte { get; set; }
         public string? DaglichtSlaapkamer { get; set; }
         public string? Roken { get; set; }
-        public string? AlchoholEnDrugs { get; set; }
         public string? VeiligeOpbergruimte { get; set; }
         public string? HuisdierenAanwezig { get; set; }
         public string? HuisdierenMogelijk { get; set; }
         public string? Allergieen { get; set; }
         public string? VluchtelingOphalen { get; set; }
         public string? BasisscholenAanwezig { get; set; }
-        public string? KinderenInDeBuurt { get; set; }
-        public string? FaciliteitenVoorKinderen { get; set; }
         public string? Beperkingen { get; set; }
         public string? AdresVanLocatie { get; set; }
         public string? PostcodeVanLocatie { get; set; }
@@ -64,7 +65,6 @@ namespace Ordina.StichtingNuTwente.Models.Models
         public string? GezinsLeeftijden { get; set; }
         public string? SlaapplaatsOpmerking { get; set; }
         public string? EigenToegangsdeur {get; set; }
-
 
         [ForeignKey("fkReactieId")]
         public virtual Reactie? Reactie { get; set; }
