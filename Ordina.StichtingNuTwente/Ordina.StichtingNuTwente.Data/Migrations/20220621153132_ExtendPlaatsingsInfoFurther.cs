2cdf95269fb77b5b34105fbd45b59a2ad992b4fd
Bas Plijnaer <basplijnaer@hotmail.com>
2022-06-22 17:27:25 +0200

    Updated filters and lists

---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Data/Migrations/20220621153132_ExtendPlaatsingsInfoFurther.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordina.StichtingNuTwente.Data.Migrations
+{
+    public partial class ExtendPlaatsingsInfoFurther : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sanitair",
+                table: "PlaatsingsInfos",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Toegankelijkheid",
+                table: "PlaatsingsInfos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sanitair",
+                table: "PlaatsingsInfos");
+
+            migrationBuilder.DropColumn(
+                name: "Toegankelijkheid",
+                table: "PlaatsingsInfos");
+        }
+    }
+}
