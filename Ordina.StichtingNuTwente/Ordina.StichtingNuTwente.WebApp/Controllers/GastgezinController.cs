7f87b28433d98b45ab1d7873bcd8a1eb7bd51ea3
Bart Klomp <bartpklomp@gmail.com>
2022-06-22 16:34:17 +0200

    Added a status filter for intakes

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.WebApp/Controllers/GastgezinController.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.WebApp/Controllers/GastgezinController.cs
@@ -589,7 +589,7 @@ namespace Ordina.StichtingNuTwente.WebApp.Controllers
         [Route("AlleGastgezinnen")]
         [HttpGet]
         [ActionName("AlleGastgezinnen")]
-        public IActionResult AlleGastgezinnen(string? sortBy = "Woonplaats", string? sortOrder = "Ascending")
+        public IActionResult AlleGastgezinnen(string? sortBy = "Woonplaats", string? sortOrder = "Ascending", string statusFilter = "")
         {
             _userService.checkIfUserExists(User);
 
@@ -606,7 +606,35 @@ namespace Ordina.StichtingNuTwente.WebApp.Controllers
                 });
             }
             var user = GetUser();
-            ICollection<Gastgezin> gastGezinnen = _gastgezinService.GetAllGastgezinnen();
+            IEnumerable<Gastgezin> gastGezinnen = _gastgezinService.GetAllGastgezinnen();
+
+            if (!string.IsNullOrEmpty(statusFilter))
+            {
+                switch (statusFilter)
+                {
+                    case "Beschikbaar":
+                        gastGezinnen = gastGezinnen.Where(g => !g.NoodOpvang && g.GetStatus() == GastgezinStatus.Bezocht);
+                        break;
+                    case "Gereserveerd":
+                        gastGezinnen = gastGezinnen.Where(g => g.GetStatus() == GastgezinStatus.Gereserveerd);
+                        break;
+                    case "Geplaatst":
+                        gastGezinnen = gastGezinnen.Where(g => g.GetStatus() == GastgezinStatus.Geplaatst);
+                        break;
+                    case "Nood":
+                        gastGezinnen = gastGezinnen.Where(g => g.NoodOpvang);
+                        break;
+                    case "On Hold":
+                        gastGezinnen = gastGezinnen.Where(g => g.OnHold);
+                        break;
+                    case "Geen Intaker":
+                        gastGezinnen = gastGezinnen.Where(g => g.Begeleider == null);
+                        break;
+                    case "Geen Buddy":
+                        gastGezinnen = gastGezinnen.Where(g => g.Buddy == null);
+                        break;
+                }
+            }
 
             foreach (var gastGezin in gastGezinnen)
             {
