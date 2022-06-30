b8d6756bbda7c189784409ad0b350c0bbef53ba6
Bas Plijnaer <basplijnaer@hotmail.com>
2022-06-30 14:33:56 +0200

    Add date

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.WebApp/Controllers/GastgezinController.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.WebApp/Controllers/GastgezinController.cs
@@ -178,7 +178,7 @@ namespace Ordina.StichtingNuTwente.WebApp.Controllers
 
         [Authorize(Policy = "RequireCoordinatorRole")]
         [Route("DeletePlaatsing")]
-        public IActionResult DeletePlaatsing(int plaatsingId, string departureReason, DepartureDestination departureDestination,  string departureComment)
+        public IActionResult DeletePlaatsing(DateTime departureDate, int plaatsingId, string departureReason, DepartureDestination departureDestination,  string departureComment)
         {
             var plaatsing = _gastgezinService.GetPlaatsing(plaatsingId);
             plaatsing.Active = false;
@@ -195,7 +195,7 @@ namespace Ordina.StichtingNuTwente.WebApp.Controllers
                 Age = plaatsing.Age,
                 AgeGroup = plaatsing.AgeGroup,
                 PlacementType = placementType,
-                DateTime = DateTime.Now,
+                DateTime = departureDate,
                 Vrijwilliger = _userService.getUserFromClaimsPrincipal(User),
                 Active = false,
                 Gender = plaatsing.Gender,
