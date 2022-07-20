83dcd413e0385f720568e80fa083fae08a63e056
Bas Plijnaer <basplijnaer@hotmail.com>
2022-07-20 14:10:01 +0200

    Added Verwijderde Plaatsingen Overview

---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Business/Services/PlaatsingenService.cs
@@ -0,0 +1,27 @@
+﻿using Ordina.StichtingNuTwente.Business.Interfaces;
+using Ordina.StichtingNuTwente.Data;
+using Ordina.StichtingNuTwente.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordina.StichtingNuTwente.Business.Services
+{
+    public class PlaatsingenService : IPlaatsingenService
+    {
+        private readonly IRepository<Plaatsing> PlaatsingenRepository;
+
+        public PlaatsingenService(IRepository<Plaatsing> plaatsingenRepository)
+        {
+            PlaatsingenRepository = plaatsingenRepository;
+        }
+
+        public ICollection<Plaatsing> GetAllPlaatsingen(PlacementType? placementType)
+        {
+            var plaatsingen = PlaatsingenRepository.GetAll("Gastgezin,Gastgezin.Contact,Vrijwilliger").Where(p => p.PlacementType == placementType);
+            return plaatsingen.ToList();
+        }
+    }
+}
