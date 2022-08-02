c7fcf57cfb64064b9c94865c91f27131ee60b9c3
Bas Plijnaer <basplijnaer@hotmail.com>
2022-08-02 16:00:59 +0200

    Added stats dropdown & "herplaatsing" option

---
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Models/ViewModels/VerwijderdePlaatsingen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ordina.StichtingNuTwente.Models.Models;
+
+namespace Ordina.StichtingNuTwente.Models.ViewModels
+{
+    public class VerwijderdePlaatsingen
+    {
+        public List<PlaatsingViewModel> plaatsingen;
+        public int total;
+        public List<plaatsingStats> plaatsingStats;
+    }
+    public class plaatsingStats
+    {
+        DepartureDestination destination;
+
+    }
+}
