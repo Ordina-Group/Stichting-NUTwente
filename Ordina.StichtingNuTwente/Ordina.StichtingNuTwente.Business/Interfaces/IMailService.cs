8aae096bd19f96e00895d4b4227362c23dbb90e3
Bas Plijnaer <basplijnaer@hotmail.com>
2022-08-03 15:30:01 +0200

    Fixed emails from forms

--- Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Business/Interfaces/IMailService.cs
+++ Ordina.StichtingNuTwente/Ordina.StichtingNuTwente.Business/Interfaces/IMailService.cs
@@ -10,15 +10,21 @@ namespace Ordina.StichtingNuTwente.Business.Interfaces
     public interface IMailService
     {
         public Task<bool> ToekennenIntaker(Gastgezin gastgezin, Persoon persoon);
-        public Task<bool> ToekennenBuddy(Gastgezin gastgezin, Persoon persoon);
-        public Task<bool> VertrekVluchteling(Gastgezin gastgezin, Persoon persoon);
-        public Task<bool> PlaatsingsReservering(Gastgezin gastgezin, Persoon persoon);
-        public Task<bool> AanmeldenVrijwilliger(Persoon persoon);
-        public Task<bool> VerwijderenGastgezin(Gastgezin gastgezin, Persoon persoon);
-        public Task<bool> IntakeUitgevoerd(Gastgezin gastgezin);
-        public Task<bool> AanmeldingVrijwilliger(Persoon persoon);
-        public Task<bool> AanmeldingGastgezin(Persoon persoon);
-        public Task<bool> PlaatsingVluchteling(Gastgezin gastgezin);
 
+        public Task<bool> ToekennenBuddy(Gastgezin gastgezin, Persoon persoon);
+
+        public Task<bool> VertrekVluchteling(Gastgezin gastgezin, Persoon persoon);
+
+        public Task<bool> PlaatsingsReservering(Gastgezin gastgezin, Persoon persoon);
+
+        public Task<bool> VerwijderenGastgezin(Gastgezin gastgezin, Persoon persoon);
+
+        public Task<bool> IntakeUitgevoerd(Gastgezin gastgezin);
+
+        public Task<bool> AanmeldingVrijwilliger(Persoon vrijwilliger);
+
+        public Task<bool> AanmeldingGastgezin(Persoon contactpersoon);
+
+        public Task<bool> PlaatsingVluchteling(Gastgezin gastgezin);
     }
 }
